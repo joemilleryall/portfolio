8677246f3652cdefa307dfd54dfd1af7b69d9e24
github username <Joemiller065@gmail.com>
2020-08-18 22:34:32 -0400

    basic modal set up for bio complete

--- js/index.js
+++ js/index.js
@@ -1,13 +1,59 @@
-const pane = document.getElementsByClassName('pane');
-const expandedPane = document.querySelector('.expandedPane');
-const bio = document.querySelector('#bio')
+const expandedPane = document.querySelector('#expandedPane');
+const timeline = document.querySelector('#timeline')
 
-bio.addEventListener('mouseover', e => {
-    if(e.target.id == "2006"){
-        expandedPane.style.display = "block"
+
+timeline.addEventListener('mouseover', e => {
+    if (e.target.dataset.id == 2006){
+        davieModal()
+    } else if (e.target.dataset.id == 2014){
+        fsuModal()
+    } else if (e.target.dataset.id == 2016){
+        thapaeModal()
+    } else if (e.target.dataset.id == 2017){
+        nycModal()
+    } else if (e.target.dataset.id == 2019){
+        flatironModal()
+    } else if (e.target.dataset.id == 2020){
+        cornellModal()
     }
 })
 
 expandedPane.addEventListener('mouseleave', e => {
     expandedPane.style.display = "none"
 })
+
+function davieModal() {
+    var divTag = document.createElement("div")
+    divTag.className = "expandedPane"
+    expandedPane.style.display = "block"
+}
+
+function fsuModal(){
+    var divTag = document.createElement("div")
+    divTag.className = "expandedPane"
+    expandedPane.style.display = "block"
+}
+
+function thapaeModal() {
+    var divTag = document.createElement("div")
+    divTag.className = "expandedPane"
+    expandedPane.style.display = "block"
+}
+
+function nycModal() {
+    var divTag = document.createElement("div")
+    divTag.className = "expandedPane"
+    expandedPane.style.display = "block"
+}
+
+function flatironModal() {
+    var divTag = document.createElement("div")
+    divTag.className = "expandedPane"
+    expandedPane.style.display = "block"
+}
+
+function cornellModal() {
+    var divTag = document.createElement("div")
+    divTag.className = "expandedPane"
+    expandedPane.style.display = "block"
+}
