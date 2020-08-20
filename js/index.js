250077080359ac3c546a05f63921ab3e88c9c5b7
github username <Joemiller065@gmail.com>
2020-08-19 21:50:17 -0400

    basic modals up and working

--- js/index.js
+++ js/index.js
@@ -19,62 +19,63 @@ timeline.addEventListener('mouseover', e => {
         cornellModal()
     }
 })
-var modalContainer = document.createElement("div")
-    modalContainer.className = "expandedPane"
 
 expandedPane.addEventListener('mouseleave', e => {
     expandedPane.style.display = "none"
+    expandedPane.innerText = ''
 })
 
 function davieModal() {
-    var modalContainer = document.createElement("div")
-    modalContainer.className = "expandedPane"
-    modalContainer.style.display = "block"
-
     var itemHeader = document.createElement("p")
     itemHeader.className = "itemHeader"
     itemHeader.innerText = "2006 - Davie, Florida"
 
-    modalContainer.appendChild(itemHeader)
-    bio.appendChild(modalContainer)
-    console.log(modalContainer)
+    expandedPane.appendChild(itemHeader)
+    expandedPane.style.display = "block"
 }
 
 
 function fsuModal(){
-    var modalContainer = document.createElement("div")
-    modalContainer.className = "expandedPane"
-    expandedPane.style.display = "block"
-
     var itemHeader = document.createElement("p")
     itemHeader.className = "itemHeader"
     itemHeader.innerText = "2014 - Tallahassee, Florida"
 
-    modalContainer.appendChild(itemHeader)
-    bio.appendChild(modalContainer)
-    console.log(modalContainer)
+    expandedPane.appendChild(itemHeader)
+    expandedPane.style.display = "block"
 }
 
 function thapaeModal() {
-    var modalContainer = document.createElement("div")
-    modalContainer.className = "expandedPane"
+    var itemHeader = document.createElement("p")
+    itemHeader.className = "itemHeader"
+    itemHeader.innerText = "2016 - Chiang Mai, Thailand"
+
+    expandedPane.appendChild(itemHeader)
     expandedPane.style.display = "block"
 }
 
 function nycModal() {
-    var modalContainer = document.createElement("div")
-    modalContainer.className = "expandedPane"
+    var itemHeader = document.createElement("p")
+    itemHeader.className = "itemHeader"
+    itemHeader.innerText = "2017 - NYC - Moved"
+
+    expandedPane.appendChild(itemHeader)
     expandedPane.style.display = "block"
 }
 
 function flatironModal() {
-    var modalContainer = document.createElement("div")
-    modalContainer.className = "expandedPane"
+    var itemHeader = document.createElement("p")
+    itemHeader.className = "itemHeader"
+    itemHeader.innerText = "2019 - Brooklyn, New York"
+
+    expandedPane.appendChild(itemHeader)
     expandedPane.style.display = "block"
 }
 
 function cornellModal() {
-    var modalContainer = document.createElement("div")
-    modalContainer.className = "expandedPane"
+    var itemHeader = document.createElement("p")
+    itemHeader.className = "itemHeader"
+    itemHeader.innerText = "2020 - NYC"
+
+    expandedPane.appendChild(itemHeader)
     expandedPane.style.display = "block"
 }
