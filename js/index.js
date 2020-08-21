9e831d43bc40cb44d70c0a4f38e89be9f43f569e
github username <Joemiller065@gmail.com>
2020-08-20 23:38:21 -0400

    bio modal v1 lookin good

--- js/index.js
+++ js/index.js
@@ -20,10 +20,6 @@ timeline.addEventListener('mouseover', e => {
     }
 })
 
-expandedPane.addEventListener('mouseleave', e => {
-    expandedPane.style.display = "none"
-    expandedPane.innerText = ''
-})
 
 briefPane.addEventListener('mouseleave', e => {
     briefPane.style.display = "none"
@@ -37,93 +33,102 @@ closeBtn.addEventListener('click', e => {
 function davieModal() {
     var itemHeader = document.createElement("p")
     itemHeader.className = "itemHeader"
-    itemHeader.innerText = "2006 - Davie, Florida"
-
-    var itemUl = document.createElement('ul')
-
-    var liOne = document.createElement('li')
-    liOne.className="itemBody"
-    liOne.innerText = "I was born and raised in Davie, Fl."
-    itemUl.appendChild(liOne)
-
-    var liTwo = document.createElement('li')
-    liTwo.innerText = "I began combat sports competition in 2005"
-    liTwo.className="itemBody"
-    itemUl.appendChild(liTwo)
-
-    var liThree = document.createElement('li')
-    liThree.innerText = "I graduated from William T. Mcfatter Tehnical Highschool in 2006"
-    liThree.className="itemBody"
-    itemUl.appendChild(liThree)
-
-
-
+    itemHeader.innerText = "~2006"
     briefPane.appendChild(itemHeader)
-    briefPane.appendChild(itemUl)
-    briefPane.appendChild(closeBtn)
 
-    closeBtn.style.display = "block"
+    var liOne = document.createElement('p')
+    liOne.className="itemBody"
+    liOne.innerText = "I was born and raised in Davie, FLorida. I left in 2006 after highschool for college in Tallahassee where I earned a Bachelor's in Editing, Writing, and Media from Florida State"
+    briefPane.appendChild(liOne)
+
+    // closeBtn.style.display = "block"
     briefPane.style.display = "block"
 
-    // const viewPane = document.querySelector('#viewPane')
-    // viewPane.innerText = ""
-
-    // const newViewPane = document.createElement('div')
-    // newViewPane.className = "newViewPane"
-
-    // pTag = document.createElement('p')
-    // pTag.className = 'viewPaneBody'
-    // pTag.innerText = "davie"
-
-    // newViewPane.appendChild(pTag)
-    // viewPane.appendChild(newViewPane)
+    briefPane.appendChild(closeBtn)
 }
 
 
 function fsuModal(){
     var itemHeader = document.createElement("p")
     itemHeader.className = "itemHeader"
-    itemHeader.innerText = "2014 - Tallahassee, Florida"
-
+    itemHeader.innerText = "2014"
     briefPane.appendChild(itemHeader)
-    briefPane.appendChild(closeBtn)
 
-    closeBtn.style.display = "block"
+    var liOne = document.createElement('p')
+    liOne.className="itemBody"
+    liOne.innerText = ""
+    briefPane.appendChild(liOne)
+
+    // closeBtn.style.display = "block"
     briefPane.style.display = "block"
+
+    briefPane.appendChild(closeBtn)
 }
 
 function thapaeModal() {
     var itemHeader = document.createElement("p")
     itemHeader.className = "itemHeader"
-    itemHeader.innerText = "2016 - Chiang Mai, Thailand"
+    itemHeader.innerText = "2016"
+    briefPane.appendChild(itemHeader)
 
-    expandedPane.appendChild(itemHeader)
-    expandedPane.style.display = "block"
+    var liOne = document.createElement('p')
+    liOne.className="itemBody"
+    liOne.innerText = ""
+    briefPane.appendChild(liOne)
+
+    // closeBtn.style.display = "block"
+    briefPane.style.display = "block"
+
+    briefPane.appendChild(closeBtn)
 }
 
 function nycModal() {
     var itemHeader = document.createElement("p")
     itemHeader.className = "itemHeader"
-    itemHeader.innerText = "2017 - NYC - Moved"
+    itemHeader.innerText = "2017"
+    briefPane.appendChild(itemHeader)
 
-    expandedPane.appendChild(itemHeader)
-    expandedPane.style.display = "block"
+    var liOne = document.createElement('p')
+    liOne.className="itemBody"
+    liOne.innerText = ""
+    briefPane.appendChild(liOne)
+
+    // closeBtn.style.display = "block"
+    briefPane.style.display = "block"
+
+    briefPane.appendChild(closeBtn)
 }
 
 function flatironModal() {
     var itemHeader = document.createElement("p")
     itemHeader.className = "itemHeader"
-    itemHeader.innerText = "2019 - Brooklyn, New York"
+    itemHeader.innerText = "2019"
+    briefPane.appendChild(itemHeader)
 
-    expandedPane.appendChild(itemHeader)
-    expandedPane.style.display = "block"
+    var liOne = document.createElement('p')
+    liOne.className="itemBody"
+    liOne.innerText = ""
+    briefPane.appendChild(liOne)
+
+    // closeBtn.style.display = "block"
+    briefPane.style.display = "block"
+
+    briefPane.appendChild(closeBtn)
 }
 
 function cornellModal() {
     var itemHeader = document.createElement("p")
     itemHeader.className = "itemHeader"
-    itemHeader.innerText = "2020 - NYC"
+    itemHeader.innerText = "2020"
+    briefPane.appendChild(itemHeader)
 
-    expandedPane.appendChild(itemHeader)
-    expandedPane.style.display = "block"
+    var liOne = document.createElement('p')
+    liOne.className="itemBody"
+    liOne.innerText = ""
+    briefPane.appendChild(liOne)
+
+    // closeBtn.style.display = "block"
+    briefPane.style.display = "block"
+
+    briefPane.appendChild(closeBtn)
 }
