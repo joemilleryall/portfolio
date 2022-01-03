9478c4bb54a3dcc383ba56d62bcea8181e08b975
github username <Joemiller065@gmail.com>
2022-01-03 08:34:25 -0500

    animated flip indicator added on intro card

---
+++ src/Calendar/Calendar.js
@@ -0,0 +1,16 @@
+
+const Calendar = () => {
+    const [viewState, setViewState] = React.useState('false');
+    return (
+        <div className="calendar-container">
+            {viewState}
+        </div>
+    );
+};
+
+ReactDOM.render(<Calendar />, document.querySelector('#calendar')); 
+
+// If i were to add all the components, I would need to add all those scripts tags to the body in the HTML. 
+// this doesnt deem ideal with all those tags. 
+
+// could I make the calendar is own repo like chase suggested - and just copy the build folder here to make it simple? is that possible?
