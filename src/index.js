6dadfe486d4f0ecf2a627e1275ec2f8639144604
github username <Joemiller065@gmail.com>
2022-01-17 15:21:53 -0500

    repo buttons in

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "../stylesheets/calendar.css";
-import "../stylesheets/animations.css";
 import "../stylesheets/index.css";
 import { Calendar } from './Calendar/Calendar.js';
 
