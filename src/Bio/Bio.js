3e2b83f2c22e75a55544cb0150a4a2e815a49ab7
github username <Joemiller065@gmail.com>
2022-01-18 15:06:40 -0500

    bio started, rest on branch

---
+++ src/Bio/Bio.js
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+// import { Month, DayModal } from '../components.js';
+// import "../../stylesheets/calendar.css";
+
+export const Bio = () => {
+    return (
+        <div className="bio-container">
+                <div className="bio">
+                    bio
+                </div>
+        </div>
+    );
+};
+
