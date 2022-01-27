1a3013209bfb887e4a0f5203f9ccaeebcfc96576
github username <Joemiller065@gmail.com>
2022-01-26 21:40:16 -0500

    bio componnet work - mostly css adding images and getting look right

--- src/Bio/Bio.js
+++ src/Bio/Bio.js
@@ -4,7 +4,14 @@ export const Bio = () => {
     return (
         <div className="bio-container">
                 <div className="bio">
-                    bio
+                    {/* <div className="img-overlay"></div> */}
+                    <div className="self ">
+                        <h4>Web Developer</h4>
+                        <div className="developer"></div>
+                    </div>
+                    <div className="self athlete">
+                        <h4>Combat Sports Athlete</h4>
+                    </div>
                 </div>
         </div>
     );
