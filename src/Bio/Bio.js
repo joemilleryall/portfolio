18a88c59db26f8ed6ffea074b6a2a79165464748
github username <Joemiller065@gmail.com>
2022-02-02 11:39:09 -0500

    bio component toggel switched working

--- src/Bio/Bio.js
+++ src/Bio/Bio.js
@@ -2,12 +2,29 @@ import { useState } from "react";
 
 export const Bio = () => {
     const [isSelected, setIsSelected] = useState('background')
+
+    const btnClick = (e) => {
+        setIsSelected(e.target.name)
+    }
+    
     return (
         <div className="bio-container">
             <div className="bio">
-                <button className={isSelected === 'background' ? 'selected' : ''}>Background</button>
-                <button className={isSelected === 'dev' ? 'selected' : ''}>Web Developer</button>
-                <button className={isSelected === 'coach' ? 'selected' : ''}>Combat Sports Coach</button>
+                <div className="component-nav">
+                    <button onClick={btnClick} name="background" className={isSelected === 'background' ? 'selected' : ''}>Background</button>
+                    <button onClick={btnClick} name="dev" className={isSelected === 'dev' ? 'selected' : ''}>Web Developer</button>
+                    <button onClick={btnClick} name="coach" className={isSelected === 'coach' ? 'selected' : ''}>Combat Sports Coach</button>
+                </div>
+
+                {isSelected === 'background' 
+                    ? <div>background</div>
+                    : isSelected === 'dev' 
+                        ? <div>dev</div>
+                        : isSelected === 'coach'
+                            ? <div>coach</div>
+                            : <div>nothing</div>
+                }
+
             </div>
         </div>
     );
