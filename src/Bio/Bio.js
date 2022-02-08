3f281d4f9023540785a46ef70793d44a705baa05
github username <Joemiller065@gmail.com>
2022-02-08 18:30:21 -0500

    some content, some functionality ideas

--- src/Bio/Bio.js
+++ src/Bio/Bio.js
@@ -1,4 +1,5 @@
 import { useRef, useEffect, useState } from "react";
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
 
 export const Bio = () => {
     const [isSelected, setIsSelected] = useState('background')
@@ -14,33 +15,44 @@ export const Bio = () => {
         setIsSelected(e.target.name)
     }
 
+
+    // The idea at the moment is to have this pane have the skills acquired and the bio points mostly about things not tech related be separated into tabs that combine im some way to make up this defauylt screen, to give an interactive bit to the user to choose which side of me they want to see more about other than just the bullets. 
+
+    // so the '+ Skills acquired' bits will maybe be a different color or have some animatino on hover to make them larger or something and a way to maybe play into the skills portion? or maybe just two different blurbs on the same pane about me biuographically conditionally rendered
+
     const bgContent = (
         <div className="bg-body opacity" ref={fadeRef}>
             <div className="timeline">
                 <ul>
-                    {/* <li>
-                        <span>2006 - </span>
-                        test content for a bio point on this portfolio
-                    </li> */}
                     <li>
-                        <span>2014 </span>
-                        <div>test content for a bio point on this portfolio and more and more and more</div>
+                        <span className="timeline-date">2006</span>
+                        <div>Graduated from McFatter Technical High School</div>
+                        <div className="skills-acquired"><span>+</span>HTML, CSS, PHP, FTP clients, Photoshop, Final Cut, and building gaming PCs</div>
                     </li>
                     <li>
-                        <span>2016 </span>
-                        <div>test content for a bio point on this portfolio and more and more and more</div>
+                        <span className="timeline-date">2014</span>
+                        {/* <div>test content for a bio point on this portfolio and more and more and more</div> */}
+                        <div>Graduated from Florida State University</div>
                     </li>
                     <li>
-                        <span>2018 </span>
-                        <div>test content for a bio point on this portfolio and more and more and more</div>
+                        <span className="timeline-date">2015</span>
+                        <div>Started  </div>
                     </li>
                     <li>
-                        <span>2019 </span>
-                        <div>test content for a bio point on this portfolio and more and more and more</div>
+                        <span className="timeline-date">2016</span>
+                        <div>Moved to Chiang Mai, Thailand to train and compete in the combat sport of <a target="_blank" href="https://www.youtube.com/watch?v=mgbGDi_ziMA">Muay Thai</a>.</div>
                     </li>
                     <li>
-                        <span>2020 </span>
-                        <div>test content for a bio point on this portfolio and more and more and more</div>
+                        <span className="timeline-date">2018</span>
+                        <div>Moved to New York City t</div>
+                    </li>
+                    <li>
+                        <span className="timeline-date">2019</span>
+                        <div>Completed Flatiron School's 15-week Immersive Software Engineering program.</div>
+                    </li>
+                    <li>
+                        <span className="timeline-date">2020</span>
+                        <div>Hired at Weill Cornell Medicine where I meet directly with stakeholders to design and fix web funcitonality based in JS, JQuery, PHP, and Drupal</div>
                     </li>
                 </ul>
             </div>
