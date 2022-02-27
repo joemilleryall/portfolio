539776efbad2d2b89b21c1eb74fac8d8c532371b
github username <Joemiller065@gmail.com>
2022-02-27 10:22:14 -0700

    starting to figure out the animated background with framer still needs work

--- src/Sitebase/Home.js
+++ src/Sitebase/Home.js
@@ -2,7 +2,7 @@ import { Nav, Gallery, Footer, Backdrop, Modal } from './components.js'
 import { motion } from 'framer-motion'
 import { useState } from 'react'
 
-// so the idea is that each one of the main componnets are single page interactive react components which do something simple but sort of clever and then there will be larger projects like creating the blogging app and the media showcase app or maybe media showcase, or whatever it may be.
+// so the idea is that each one of the main componnets are single page interactive react components which do something simple but sort of clever and then there will be larger projects divke creating the blogging app and the media showcase app or maybe media showcase, or whatever it may be.
 
 export const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -12,6 +12,98 @@ export const Home = () => {
 
     return (
         <div className="home">
+        
+            <motion.div
+                className="star"
+                animate={{ 
+                    y:['0vh', '-100vh'], 
+                    scale: [1, 3, 1], 
+                    rotate: 720,
+                }}
+                transition={{ duration: 4, repeat: Infinity, delay: 1 }}
+            />
+            <motion.div
+                className="star"
+                animate={{ 
+                    y:['0vh', '-100vh'], 
+                    scale: [1, 3, 1], 
+                    rotate: 720,
+                }}
+                transition={{ duration: 4, repeat: Infinity, delay: 2 }}
+            />
+            <motion.div
+                className="star"
+                animate={{ 
+                    y:['0vh', '-100vh'], 
+                    scale: [1, 3, 1], 
+                    rotate: 720,
+                }}
+                transition={{ duration: 4, repeat: Infinity, delay: 3 }}
+            />
+            <motion.div
+                className="star"
+                animate={{ 
+                    y:['0vh', '-100vh'], 
+                    scale: [1, 3, 1], 
+                    rotate: 720,
+                }}
+                transition={{ duration: 4, repeat: Infinity, delay: 4 }}
+            />
+            <motion.div
+                className="star"
+                animate={{ 
+                    y:['0vh', '-100vh'], 
+                    scale: [1, 3, 1], 
+                    rotate: 720,
+                }}
+                transition={{ duration: 4, repeat: Infinity, delay: 0.5 }}
+            />
+            <motion.div
+                className="star"
+                animate={{ 
+                    y:['0vh', '-100vh'], 
+                    scale: [1, 3, 1], 
+                    rotate: 720,
+                }}
+                transition={{ duration: 4, repeat: Infinity, delay: 0.6 }}
+            />
+            <motion.div
+                className="star"
+                animate={{ 
+                    y:['0vh', '-100vh'], 
+                    scale: [1, 3, 1], 
+                    rotate: 720,
+                }}
+                transition={{ duration: 4, repeat: Infinity, delay: 0.7 }}
+            />
+            <motion.div
+                className="star"
+                animate={{ 
+                    y:['0vh', '-100vh'], 
+                    scale: [1, 3, 1], 
+                    rotate: 720,
+                }}
+                transition={{ duration: 4, repeat: Infinity, delay: 0.8 }}
+            />
+            <motion.div
+                className="star"
+                animate={{ 
+                    y:['0vh', '-100vh'], 
+                    scale: [1, 3, 1], 
+                    rotate: 720,
+                }}
+                transition={{ duration: 4, repeat: Infinity, delay: 0.9 }}
+            />
+            <motion.div
+                className="star"
+                animate={{ 
+                    y:['0vh', '-100vh'], 
+                    scale: [1, 3, 1], 
+                    rotate: 720,
+                }}
+                transition={{ duration: 4, repeat: Infinity, delay: 1 }}
+            />
+
             <Nav open={open} />
             <Gallery isOpen={isOpen} close={close} />
         </div>
--- src/Sitebase/components/Gallery.js
+++ src/Sitebase/components/Gallery.js
@@ -1,16 +1,36 @@
 import { Backdrop, Modal } from '../components.js'
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
+import uniqid from 'uniqid'
 
 export const Gallery = ({ isOpen, close }) => {
     // const [isOpen, setIsOpen] = useState(false);
-    
+    const [selectedId, setSelectedId] = useState(null)
+
+    // const items = [
+    //     {
+    //         id: uniqid(),
+    //         subtitle: "subtitle1",
+    //         title: "title1"
+    //     },
+    //     {
+    //         id: uniqid(),
+    //         subtitle: "subtitle2",
+    //         title: "title2"
+    //     },
+    //     {
+    //         id: uniqid(),
+    //         subtitle: "subtitle3",
+    //         title: "title3"
+    //     },
+    //     {
+    //         id: uniqid(),
+    //         subtitle: "subtitle4",
+    //         title: "title4"
+    //     },
+    // ]
     return (
         <div className="gallery">
-            <motion.div className="bg "></motion.div>
-            <motion.div className="bg "></motion.div>
-            <motion.div className="bg "></motion.div>
-            <motion.div className="bg"></motion.div>
             <AnimatePresence>
                 {isOpen && <Backdrop onClick={close}><Modal isOpen={isOpen} handleClose={close} /></Backdrop>}
             </AnimatePresence>
