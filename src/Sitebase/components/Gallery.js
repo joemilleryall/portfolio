9a54915ec4caae89b1015d8ac85a668cd3f5c06f
github username <Joemiller065@gmail.com>
2022-04-04 09:18:51 -0400

    some tooling for look

--- src/Sitebase/components/Gallery.js
+++ src/Sitebase/components/Gallery.js
@@ -8,6 +8,12 @@ export const Gallery = ({ isOpen, close }) => {
 
     return (
         <div className="gallery">
+            {/* <div className="block-1"></div>
+            <div className="block-2"></div>
+            <div className="block-3"></div>
+            <div className="block-4"></div>
+            <div className="block-5"></div>
+            <div className="block-6"></div> */}
             <AnimatePresence>
                 {isOpen && <Backdrop onClick={close}><Modal isOpen={isOpen} handleClose={close} /></Backdrop>}
             </AnimatePresence>
--- src/Sitebase/components/Nav.js
+++ src/Sitebase/components/Nav.js
@@ -24,9 +24,6 @@ export const Nav = ({ open }) => {
         }
     }
 
-    const btnFunc = () => {
-        
-    }
 
     return (
         <nav>
@@ -44,67 +41,69 @@ export const Nav = ({ open }) => {
                             animate="visible"
                             exit="exit"
                         >
-                        <motion.button
-                            onClick={() => { setIsNavOpen(false) }}
-                            className="close-nav"
-                            key="close-nav-btn"
-                            whileHover={{ scale: 1.3, borderRadius: "100%", backgroundColor: "rgb(0, 0, 0)" }}
-                            whileTap={{ scale: 0.7 }}
-                            initial={{opacity: 0}}
-                            animate={{opacity: 1}}
-                            exit={{opacity: 0}}
+                            <motion.button
+                                onClick={() => { setIsNavOpen(false) }}
+                                className="close-nav"
+                                key="close-nav-btn"
+                                whileHover={{ scale: 1.3, borderRadius: "100%", backgroundColor: "rgb(0, 0, 0)" }}
+                                whileTap={{ scale: 0.7 }}
+                                initial={{opacity: 0}}
+                                animate={{opacity: 1}}
+                                exit={{opacity: 0}}
+                            >
+                                &#x2936;
+                            </motion.button>
+                            <motion.button
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.7 }}
+                                className="nav-btn"
+                                onClick={open}
+                            >
+                                Open
+                            </motion.button>
+                            <motion.button
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.9 }}
+                                className="nav-btn"
+                                onClick={() => null}
+                            >
+                                Button
+                            </motion.button>
+                            <motion.button
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.9 }}
+                                className="nav-btn"
+                                onClick={() => null}
+                            >
+                                Button
+                            </motion.button>
+                            <motion.button
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.9 }}
+                                className="nav-btn"
+                                onClick={() => null}
+                            >
+                                .json?
+                                {/* https://www.reddit.com/r/UkrainianConflict/new.json */}
+                            </motion.button>
+                        </motion.div>) 
+                    : (
+                        <motion.div
+                            key="open-nav-btn"
+                            onClick={() => { setIsNavOpen(true) }}
+                            className="open-nav"
+                            whileHover={{ scale:1.5 }}
+                            variants={slideOut}
+                            initial="hidden"
+                            animate="visible"
+                            exit="exit"
+                            
                         >
-                            &#x2936;
-                        </motion.button>
-                        <motion.button
-                            whileHover={{ scale: 1.1 }}
-                            whileTap={{ scale: 0.7 }}
-                            className="nav-btn"
-                            onClick={open}
-                        >
-                            Open
-                        </motion.button>
-                        <motion.button
-                            whileHover={{ scale: 1.1 }}
-                            whileTap={{ scale: 0.9 }}
-                            className="nav-btn"
-                            onClick={() => null}
-                        >
-                            Button
-                        </motion.button>
-                        <motion.button
-                            whileHover={{ scale: 1.1 }}
-                            whileTap={{ scale: 0.9 }}
-                            className="nav-btn"
-                            onClick={() => null}
-                        >
-                            Button
-                        </motion.button>
-                        <motion.button
-                            whileHover={{ scale: 1.1 }}
-                            whileTap={{ scale: 0.9 }}
-                            className="nav-btn"
-                            onClick={() => null}
-                        >
-                            .json?
-                            {/* https://www.reddit.com/r/UkrainianConflict/new.json */}
-                        </motion.button>
-                    </motion.div>) 
-                    : (<motion.div
-                        key="open-nav-btn"
-                        onClick={() => { setIsNavOpen(true) }}
-                        className="open-nav"
-                        whileHover={{ scale:1.5 }}
-                        variants={slideOut}
-                        initial="hidden"
-                        animate="visible"
-                        exit="exit"
-                        
-                    >
-                        <motion.div whileHover={{ scale: 1.2 }} key="top"></motion.div>
-                        <motion.div whileHover={{ scale: 1.2 }} key="middle"></motion.div>
-                        <motion.div whileHover={{ scale: 1.2 }} key="bottom"></motion.div>
-                    </motion.div>)
+                            <motion.div whileHover={{ scale: 1.2 }} key="top"></motion.div>
+                            <motion.div whileHover={{ scale: 1.2 }} key="middle"></motion.div>
+                            <motion.div whileHover={{ scale: 1.2 }} key="bottom"></motion.div>
+                        </motion.div>
+                    )
                 }
             </AnimatePresence>
         </nav>
